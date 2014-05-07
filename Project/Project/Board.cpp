be6916978821112b57053ab6ce8f45c168d95c38
aheacock <aheacock@vt.edu>
2014-05-07 14:46:13 -0400

    Added Win checking for large board.

--- Project/Project/Board.cpp
+++ Project/Project/Board.cpp
@@ -71,7 +71,8 @@ bool Board::checkDraw() const
 	bool returned = false;
 	if (board[0][0] != '1' && board[0][1] != '2' && board[0][2] != '3'
 		&& board[1][0] != '4' && board[1][1] != '5' && board[1][2] != '6'
-		&& board[2][0] != '7' && board[2][1] != '8' && board[2][2] != '9')
+		&& board[2][0] != '7' && board[2][1] != '8' && board[2][2] != '9'
+		&& checkWinner() == ' ')
 	{
 		returned = true;
 	}
--- Project/Project/LargeBoard.cpp
+++ Project/Project/LargeBoard.cpp
@@ -5,3 +5,48 @@ LargeBoard::LargeBoard()
 {
 	
 }
+
+
+
+char LargeBoard::checkWinner() const
+{
+	char returned = ' ';
+	if (board[0][0].checkWinner() == board[0][1].checkWinner() && board[0][1].checkWinner() == board[0][2].checkWinner())
+
+		returned = board[0][0].checkWinner();
+	else if (board[1][0].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[1][2].checkWinner())
+
+		returned = board[1][0].checkWinner();
+	else if (board[2][0].checkWinner() == board[2][1].checkWinner() && board[2][1].checkWinner() == board[1][2].checkWinner())
+
+		returned = board[2][0].checkWinner();
+	else if (board[0][0].checkWinner() == board[1][0].checkWinner() && board[1][0].checkWinner() == board[2][0].checkWinner())
+
+		returned = board[0][0].checkWinner();
+	else if (board[0][1].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[2][1].checkWinner())
+
+		returned = board[0][1].checkWinner();
+	else if (board[0][2].checkWinner() == board[1][2].checkWinner() && board[1][2].checkWinner() == board[2][2].checkWinner())
+
+		returned = board[0][2].checkWinner();
+	else if (board[0][0].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[2][2].checkWinner())
+
+		returned = board[0][0].checkWinner();
+	else if (board[0][2].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[2][0].checkWinner())
+
+		returned = board[0][2].checkWinner();
+	return returned;
+}
+
+bool LargeBoard::checkDraw() const
+{
+	bool returned = false;
+	if (   board[0][0].checkWinner() != ' ' && board[0][1].checkWinner() != ' ' && board[0][2].checkWinner() != ' '
+		&& board[1][0].checkWinner() != ' ' && board[1][1].checkWinner() != ' ' && board[1][2].checkWinner() != ' '
+		&& board[2][0].checkWinner() != ' ' && board[2][1].checkWinner() != ' ' && board[2][2].checkWinner() != ' '
+		&& checkWinner() == ' ')
+	{
+		returned = true;
+	}
+	return returned;
+}
